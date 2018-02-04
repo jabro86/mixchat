acf0135f8160d49cceef7bc523f1760e914cca7b
Jan Brockmeyer <jan.brockmeyer@mgm-tp.com>
2018-02-04 21:35:11 +0100

    Handling Register Form Errors in React Using Semantic UI and GraphQL

--- src/routes/Register.tsx
+++ src/routes/Register.tsx
@@ -1,18 +1,38 @@
 import * as React from "react";
-import { Container, Header, Input, Button } from "semantic-ui-react";
+import { Container, Header, Input, Button, Message } from "semantic-ui-react";
 import { graphql, ChildProps } from "react-apollo";
+import { RouteComponentProps } from "react-router-dom";
 import gql from "graphql-tag";
 
 interface RegisterState {
 	[key: string]: string;
 }
 
-class Register extends React.Component<ChildProps<{}, {}>, RegisterState> {
+interface RegisterProps extends RouteComponentProps<{}> { }
+
+export interface Error {
+	path: string;
+	message: string;
+}
+
+export interface RegisterResponse {
+	ok: boolean;
+	errors: Error[];
+}
+
+export interface RegisterMutation {
+	register: RegisterResponse;
+}
+
+class Register extends React.Component<ChildProps<RegisterProps, RegisterMutation>, RegisterState> {
 
 	state = {
 		username: "",
+		usernameError: "",
 		email: "",
-		password: ""
+		emailError: "",
+		password: "",
+		passwordError: ""
 	};
 
 	onChange = (e: React.SyntheticEvent<HTMLInputElement>) => {
@@ -23,22 +43,61 @@ class Register extends React.Component<ChildProps<{}, {}>, RegisterState> {
 	}
 
 	onSubmit = async () => {
+
+		this.setState({
+			usernameError: "",
+			emailError: "",
+			passwordError: ""
+		});
+
 		if (this.props.mutate) {
+			const { username, email, password } = this.state;
 			const response = await this.props.mutate({
-				variables: this.state
+				variables: { username, email, password }
 			});
+
+			const { ok, errors } = response.data.register;
+			if (ok) {
+				this.props.history.push("/");
+			} else {
+				const err = {};
+				errors.forEach(({ path, message }) => {
+					err[`${path}Error`] = message;
+				});
+				this.setState({ ...err });
+			}
+
 			console.log(response); // tslint:disable-line
 		}
 	}
 
 	render() {
 
-		const { username, email, password } = this.state;
+		const {
+			username,
+			usernameError,
+			email,
+			emailError,
+			password,
+			passwordError
+		} = this.state;
+
+		const errorList = [];
+		if (usernameError) {
+			errorList.push(usernameError);
+		}
+		if (emailError) {
+			errorList.push(emailError);
+		}
+		if (passwordError) {
+			errorList.push(passwordError);
+		}
 
 		return (
 			<Container>
 				<Header as="h2">Register</Header>
 				<Input
+					error={!!usernameError}
 					name="username"
 					onChange={this.onChange}
 					value={username}
@@ -46,6 +105,7 @@ class Register extends React.Component<ChildProps<{}, {}>, RegisterState> {
 					fluid={true}
 				/>
 				<Input
+					error={!!emailError}
 					name="email"
 					onChange={this.onChange}
 					value={email}
@@ -53,6 +113,7 @@ class Register extends React.Component<ChildProps<{}, {}>, RegisterState> {
 					fluid={true}
 				/>
 				<Input
+					error={!!passwordError}
 					name="password"
 					onChange={this.onChange}
 					value={password}
@@ -61,6 +122,16 @@ class Register extends React.Component<ChildProps<{}, {}>, RegisterState> {
 					fluid={true}
 				/>
 				<Button onClick={this.onSubmit}>Submit</Button>
+				{usernameError || emailError || passwordError
+					? (
+						<Message
+							error={true}
+							header="There was some errors with your submission"
+							list={errorList}
+						/>
+					)
+					: null
+				}
 			</Container>
 		);
 	}
@@ -68,8 +139,14 @@ class Register extends React.Component<ChildProps<{}, {}>, RegisterState> {
 
 const registerMutation = gql`
 	mutation ($username: String!, $email: String!, $password: String!) {
-		register(username: $username, email: $email, password: $password)
+		register(username: $username, email: $email, password: $password) {
+			ok
+			errors {
+				path
+				message
+			}
+		}
   	}
 `;
 
-export default graphql(registerMutation)(Register);
+export default graphql<RegisterMutation>(registerMutation)(Register);
