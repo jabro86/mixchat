4f74ae4c89e8cb116e5b08e4cd19a130ed9587b1
Jan Brockmeyer <jan.brockmeyer@mgm-tp.com>
2018-02-05 22:48:07 +0100

    Setup login form mit mobx and react

---
+++ src/routes/Login.tsx
@@ -0,0 +1,59 @@
+import * as React from "react";
+import { observer } from "mobx-react";
+import { extendObservable } from "mobx";
+import { Container, Header, Input, Button } from "semantic-ui-react";
+
+interface LoginProps {
+
+}
+
+export default observer(class Login extends React.Component<LoginProps> {
+
+	private email: string;
+	private password: string;
+
+	constructor(props: LoginProps) {
+		super(props);
+
+		extendObservable(this, {
+			email: "",
+			password: ""
+		});
+	}
+
+	onChange = (event: React.SyntheticEvent<HTMLInputElement>) => {
+		const { name, value } = event.currentTarget;
+		this[name] = value;
+	}
+
+	onSubmit = () => {
+		const { email, password } = this;
+		console.log(email, password); // tslint:disable-line
+	}
+
+	render() {
+		const { email, password } = this;
+		return (
+			<Container>
+				<Header as="h2">Register</Header>
+				<Input
+					name="email"
+					onChange={this.onChange}
+					value={email}
+					placeholder="Email"
+					fluid={true}
+				/>
+				<Input
+					name="password"
+					onChange={this.onChange}
+					value={password}
+					type="password"
+					placeholder="Password"
+					fluid={true}
+				/>
+				<Button onClick={this.onSubmit}>Submit</Button>
+			</Container>
+		);
+	}
+
+});
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./Home";
 import Register from "./Register";
+import Login from "./Login";
 
 export default () => {
 	return (
@@ -9,6 +10,7 @@ export default () => {
 			<Switch>
 				<Route path="/" exact={true} component={Home} />
 				<Route path="/register" exact={true} component={Register} />
+				<Route path="/login" exact={true} component={Login} />
 			</Switch>
 		</BrowserRouter>
 	);
