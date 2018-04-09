a31c466fc26cae8937ee5bb71aac55402fac9094
Jan Brockmeyer <jan.brockmeyer@mgm-tp.com>
2018-04-09 22:19:25 +0200

    Semantic UI React Comment Component

--- src/components/Messages.tsx
+++ src/components/Messages.tsx
@@ -4,6 +4,8 @@ import styled from "styled-components";
 const MessagesWrapper = styled.div`
 	grid-column: 3;
 	grid-row: 2;
+	padding-left: 20px;
+	padding-right: 20px;
 `;
 
 // tslint:disable-next-line:no-any
--- src/containers/MessageContainer.tsx
+++ src/containers/MessageContainer.tsx
@@ -1,12 +1,32 @@
 import * as React from "react";
 import gql from "graphql-tag";
 import { graphql } from "react-apollo";
+import { Comment } from "semantic-ui-react";
 
 import Messages from "../components/Messages";
 
-// tslint:disable-next-line:no-any
+// tslint:disable:no-any
 const MessageContainer = ({ data: { loading, messages } }: any) =>
-	loading ? null : <Messages>{JSON.stringify(messages)}</Messages>;
+	loading ? null : (
+		<Messages>
+			<Comment.Group>
+				{messages.map((m: any) => (
+					<Comment key={`${m.id}-message`}>
+						<Comment.Content>
+							<Comment.Author as="a">{m.user.username}</Comment.Author>
+							<Comment.Metadata>
+								<div>{m.created_at}</div>
+							</Comment.Metadata>
+							<Comment.Text>{m.text}</Comment.Text>
+							<Comment.Actions>
+								<Comment.Action>Reply</Comment.Action>
+							</Comment.Actions>
+						</Comment.Content>
+					</Comment>
+				))}
+			</Comment.Group>
+		</Messages>
+	);
 
 const messagesQuery = gql`
 	query($channelId: Int!) {
@@ -16,7 +36,7 @@ const messagesQuery = gql`
 			user {
 				username
 			}
-			createdAt
+			created_at
 		}
 	}
 `;
