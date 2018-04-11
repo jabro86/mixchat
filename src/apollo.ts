556992eb44dd4a15be27d0b156bb867ecc4acd81
Jan Brockmeyer <jan.brockmeyer@mgm-tp.com>
2018-04-12 00:49:14 +0200

    GraphQL Subscriptions Authentication

--- src/apollo.ts
+++ src/apollo.ts
@@ -41,7 +41,11 @@ const httpLinkWithMiddleware = afterwareLink.concat(authLink.concat(httpLink));
 const wsLink = new WebSocketLink({
 	uri: "ws://localhost:8080/subscriptions",
 	options: {
-		reconnect: true
+		reconnect: true,
+		connectionParams: {
+			token: localStorage.getItem("token"),
+			refreshToken: localStorage.getItem("refreshToken")
+		}
 	}
 });
 
